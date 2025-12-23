7af109f9db143327f408de4faeb8ebce5e4c9092
IskXCr <IskXCr@outlook.com>
2025-12-23 13:45:07 +0000

    feat: visual hull

---
+++ utils/init_utils.py
@@ -0,0 +1,57 @@
+import torch
+import torchhull
+import kaolin
+import numpy as np
+
+from scene.cameras import Camera
+from .graphics_utils import getProjectionMatrix
+from .render_utils import focus_point_fn
+
+import torch
+
+# Transform z coords from [0, 1] to [-1, 1]
+__builtin_NDC_transform_M = torch.tensor([
+    [1., 0., 0., 0.],
+    [0., 1., 0., 0.],
+    [0., 0., 2., -1.],
+    [0., 0., 0., 1.],
+], device="cuda", dtype=torch.float32)
+
+def extract_vh_args_from_cameras(cameras: list[Camera]):
+    lst_0 = [(cam.gt_alpha_mask.permute(1, 2, 0).cuda(), cam.full_proj_transform.T) for cam in cameras]
+    mask_lst, transform_lst = zip(*lst_0)
+    masks, transforms = torch.stack(mask_lst, dim=0), torch.stack(transform_lst, dim=0)
+    transforms = __builtin_NDC_transform_M[None] @ transforms
+
+    return masks, transforms
+
+def estimate_bounding_sphere(cameras: list[Camera]):
+    """
+    Estimate the bounding sphere given camera pose
+    """
+    torch.cuda.empty_cache()
+    c2ws = np.array([np.linalg.inv(np.asarray((cam.world_view_transform.T).cpu().numpy())) for cam in cameras])
+    poses = c2ws[:,:3,:] @ np.diag([1, -1, -1, 1])
+    center = (focus_point_fn(poses))
+    radius = np.linalg.norm(c2ws[:,:3,3] - center, axis=-1).min()
+    return center.tolist(), radius
+
+def compute_visual_hull(
+    masks: torch.Tensor,
+    transforms: torch.Tensor,
+    camera_center: torch.Tensor,
+    radius: torch.Tensor,
+    level: int=11,
+    masks_partial: bool=False
+) -> tuple[torch.Tensor, torch.Tensor]:
+    verts, faces = torchhull.visual_hull(
+        masks,  # [B, H, W, 1]
+        transforms,  # [B, 4, 4]
+        level,
+        [camera_center[0] - radius, camera_center[1] - radius, camera_center[2] - radius],
+        radius * 2,
+        masks_partial=masks_partial,
+        transforms_convention="opengl",
+        unique_verts=True,
+    )
+    return verts, faces
---
+++ visual_hull.py
@@ -0,0 +1,85 @@
+#
+# Copyright (C) 2023, Inria
+# GRAPHDECO research group, https://team.inria.fr/graphdeco
+# All rights reserved.
+#
+# This software is free for non-commercial, research and evaluation use 
+# under the terms of the LICENSE.md file.
+#
+# For inquiries contact  george.drettakis@inria.fr
+#
+
+import os
+import torch
+import math
+from random import randint
+from gaussian_renderer import render, network_gui
+import sys
+from scene import Scene, GaussianModel
+from scene.cameras import Camera
+from utils.general_utils import safe_state
+import uuid
+from tqdm import tqdm
+from argparse import ArgumentParser, Namespace
+from arguments import ModelParams, PipelineParams, OptimizationParams
+from utils.init_utils import extract_vh_args_from_cameras, compute_visual_hull, estimate_bounding_sphere
+import kaolin
+
+def prepare_output_and_logger(args):    
+    if not args.model_path:
+        if os.getenv('OAR_JOB_ID'):
+            unique_str=os.getenv('OAR_JOB_ID')
+        else:
+            unique_str = str(uuid.uuid4())
+        args.model_path = os.path.join("./output/", unique_str[0:10])
+        
+    # Set up output folder
+    print("Output folder: {}".format(args.model_path))
+    os.makedirs(args.model_path, exist_ok = True)
+    with open(os.path.join(args.model_path, "cfg_args"), 'w') as cfg_log_f:
+        cfg_log_f.write(str(Namespace(**vars(args))))
+
+def main(dataset, opt, pipe):
+    gaussians = GaussianModel(dataset.sh_degree)
+    scene = Scene(dataset, gaussians)
+    cams = scene.getTrainCameras().copy()
+    print("Running extraction...")
+    masks, transforms = extract_vh_args_from_cameras(cams)
+    # print(masks.shape)
+    # print(transforms.shape)
+    print("Estimating bounding sphere")
+    center, radius = estimate_bounding_sphere(cams)
+    print(f"Center={center}, radius={radius}")
+    print("Computing visual hull...")
+    verts, faces = compute_visual_hull(masks, transforms, center, radius)
+    print(verts.shape)
+    # print(verts.dtype)
+    print(faces.shape)
+    # print(faces.dtype)
+    
+    print("Saving USD...")
+    kaolin.io.usd.export_mesh(
+        "./new_stage.usd",
+        vertices=verts,
+        faces=faces,
+        overwrite=True,          # set True if you want to overwrite
+    )
+
+    print("Complete")
+
+if __name__ == "__main__":
+    # Set up command line argument parser
+    parser = ArgumentParser(description="Training script parameters")
+    lp = ModelParams(parser)
+    op = OptimizationParams(parser)
+    pp = PipelineParams(parser)
+    args = parser.parse_args(sys.argv[1:])
+    
+    # Initialize system state (RNG)
+    safe_state(None)
+    os.makedirs(args.model_path, exist_ok = True)
+
+    # Start GUI server, configure and run training
+    dataset, opt, pipe = lp.extract(args), op.extract(args), pp.extract(args)
+    prepare_output_and_logger(dataset)
+    main(dataset, opt, pipe)
