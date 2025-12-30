864ee21243a73379ce7a0de282d18bbe47d876b2
IskXCr <IskXCr@outlook.com>
2025-12-30 10:35:04 +0000

    update

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -104,7 +104,7 @@ class OptimizationParams(ParamGroup):
         self.densify_grad_threshold = 0.0001
         self.prune_floaters_from_iter = 15_000
         self.prune_floaters_until_iter = 30_000
-        self.prune_floaters_interval = 5000
+        self.prune_floaters_interval = 27_000
         self.prune_floaters_eps = 5e-2
         super().__init__(parser, "Optimization Parameters")
 
